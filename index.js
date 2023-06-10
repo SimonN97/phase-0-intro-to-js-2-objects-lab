5a6540565d2fc2ac7dc26c77b490ad88d45b002b
Simon Njuguna Wanjira <snwnjuguna97@gmail.com>
2023-06-10 22:54:09 +0300

    Finished Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee ={name: "Simon", streetAddress: "Nairobi"};
+console.log(employee)
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    const newEmployee={...employee};
+    newEmployee[key]=value;
+    console.log(newEmployee);
+    return newEmployee;
+}
+updateEmployeeWithKeyAndValue(employee,"name","Wanjira");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+    employee[key]=value;
+    console.log(employee);
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee,"name","Njuguna");
+
+function deleteFromEmployeeByKey(employee,key) {
+    const nwemployee={...employee};
+    delete nwemployee[key];
+    console.log(nwemployee);
+    return nwemployee;
+}
+deleteFromEmployeeByKey(employee,"streetAddress");
+
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+    delete employee[key];
+    console.log(employee);
+    return employee;
+}
+destructivelyDeleteFromEmployeeByKey(employee,"name")
